9d75d0753d0cf2f433259e1f027e488698971131
pratyush7625077 <143022015+pratyush7625077@users.noreply.github.com>
2025-06-24 13:43:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function firstWord(s) {
   // your code here
+	let i = 0;
+	while(i < s.length && s[i] === ' '){
+		i++;
+	}
+	let start = i;
+	while(i < s.length && s[i] !== ' '){
+		i++;
+	}
+	return s.substring(start, i);
 }
+
 
 // Do not change the code below
 
